79fa6c29f418525f62614f9199750275ea097a25
Nigel David <nigel.david@founderandlightning.com>
2026-03-24 14:47:48 +0530

    Remove everything

--- app/tasks/page.tsx
+++
@@ -1,9 +0,0 @@
-import TasksBoard from '../../components/TasksBoard'
-export default function Tasks(){
-  return (
-    <div>
-      <h1 className="text-3xl font-bold">Tasks</h1>
-      <TasksBoard />
-    </div>
-  )
-}
--- components/ui/panel.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export function Panel({ children, className='' }: { children?: React.ReactNode, className?: string }){
-  return <div className={"panel rounded-2xl p-4 " + className}>{children}</div>
-}
-
-export default Panel
